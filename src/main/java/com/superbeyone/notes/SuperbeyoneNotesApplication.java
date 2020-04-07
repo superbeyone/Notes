d296594ef0123abefda5334de3afdb5f8a154a89
superbeyone <superbeyone@outlook.com>
2020-04-07 13:57:37 +0800

    project init

---
+++ src/main/java/com/superbeyone/notes/SuperbeyoneNotesApplication.java
@@ -0,0 +1,13 @@
+package com.superbeyone.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbeyoneNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperbeyoneNotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/superbeyone/notes/SuperbeyoneNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superbeyone.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbeyoneNotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
